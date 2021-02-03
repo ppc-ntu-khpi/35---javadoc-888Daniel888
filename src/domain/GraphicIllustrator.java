4357e77e3d7790ca201fa424d04a260a3f0afa20
888Daniel888 <58972003+888Daniel888@users.noreply.github.com>
2021-02-03 18:03:35 +0300

    Add files via upload

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,8 @@
 package domain;
+
+/**
+ * Подкласс Artist, представляющий графического иллюстратора
+ * @see Artist
+ * @author 888Daniel888 */
 public class GraphicIllustrator extends Artist {
 }
--- src/domain/TechnicalWriter.java
+++ src/domain/TechnicalWriter.java
@@ -1,3 +1,9 @@
 package domain;
+
+/**
+ * Подкласс Artist, представляющий технического писателя.
+ * @see Artist
+ * @author 888Daniel888
+ */
 public class TechnicalWriter extends Artist {
 }
